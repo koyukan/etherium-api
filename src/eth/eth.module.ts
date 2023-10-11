74327a1b3648b60fdfdadd7a610a1639d79220b0
Kaan Koyukan <kaankoyukan@anayurtteknoloji.com>
2023-10-11 16:52:25 +0300

    Not Working

--- src/eth/eth.module.ts
+++ src/eth/eth.module.ts
@@ -3,10 +3,19 @@ import { EthController } from './eth.controller';
 import { EthService } from './eth.service';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   controllers: [EthController],
-  imports: [HttpModule, ConfigModule.forRoot()],
+  imports: [
+    HttpModule,
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      store: 'memory',
+      ttl: 60 * 1000, // milliseconds
+      max: 5, // maximum number of items in cache
+    }),
+  ],
   providers: [EthService],
 })
 export class EthModule {}
--- src/eth/eth.service.ts
+++ src/eth/eth.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, Inject } from '@nestjs/common';
 import HttpProvider from 'web3-providers-http';
 import Eth from 'web3-eth';
 import { fromWei, Web3DeferredPromise } from 'web3-utils';
@@ -6,24 +6,61 @@ import { isAddress } from 'web3-validator';
 import { HttpService } from '@nestjs/axios';
 import { map, catchError, lastValueFrom } from 'rxjs';
 import { JsonRpcOptionalRequest } from 'web3-types';
+import { Contract } from 'web3-eth-contract';
 import { ConfigService } from '@nestjs/config';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+
 @Injectable()
 export class EthService {
   private eth: Eth;
+  private usdtContract: any;
 
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {
     // Initialize Eth instance; here it's connected to the Ethereum mainnet via Infura
     const provider = new HttpProvider(configService.get<string>('INFURA_URL'));
     this.eth = new Eth(provider);
+    const balanceOfABI = [
+      {
+        constant: true,
+        inputs: [
+          {
+            name: '_owner',
+            type: 'address',
+          },
+        ],
+        name: 'balanceOf',
+        outputs: [
+          {
+            name: 'balance',
+            type: 'uint256',
+          },
+        ],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ];
+    const tokenContract = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+    this.usdtContract = new Contract(balanceOfABI, tokenContract);
+    this.usdtContract.setProvider(provider);
   }
 
   // Fetches the current ETH/USD price from CoinGecko
   private async fetchEthToUsdPrice(): Promise<number> {
-    const url = this.configService.get<string>('COINGECKO_URL');
+    const cacheKey = 'ethToUsdPrice';
+    const cachedPrice: number = await this.cacheManager.get(cacheKey);
 
+    if (cachedPrice) {
+      return cachedPrice;
+    }
+
+    console.log('Fetching ETH/USD price');
+    const url = this.configService.get<string>('COINGECKO_URL');
     const request = this.httpService.get(url).pipe(
       map((response) => response.data.ethereum.usd),
       catchError(() => {
@@ -34,6 +71,7 @@ export class EthService {
     );
 
     const price = await lastValueFrom(request);
+    await this.cacheManager.set(cacheKey, price);
 
     return price;
   }
@@ -59,12 +97,13 @@ export class EthService {
     addresses.forEach((address) => {
       if (isAddress(address, true)) {
         validAddresses.push(address);
-        const payload: JsonRpcOptionalRequest = {
+
+        // ETH balance payload
+        const ethPayload: JsonRpcOptionalRequest = {
           method: 'eth_getBalance',
           params: [address, 'latest'],
         };
-        const deferredPromise = batch.add(payload);
-        balancePromises.push(deferredPromise);
+        balancePromises.push(batch.add(ethPayload));
       } else {
         invalidAddresses.push(address);
       }
@@ -88,6 +127,19 @@ export class EthService {
       })
       // Sort the results by USD balance in descending order
       .sort((a, b) => b.usd_balance - a.usd_balance);
+
+    // USDT balance payload
+    const batch2 = new this.eth.BatchRequest();
+    const balancePromises2: Web3DeferredPromise<unknown>[] = [];
+    usdtPayload: JsonRpcOptionalRequest = {
+      method: 'eth_call',
+      params: [address, 'latest'],
+    };
+    const resul = await this.usdtContract.methods
+      .balanceOf(addresses[0])
+      .call();
+    console.log(resul);
+    balancePromises.push(batch.add(resul));
 
     return {
       wrong_addresses: invalidAddresses,
